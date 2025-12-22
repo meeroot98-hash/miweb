1abdbd7456ae419516693085413ac976c2b54daf
Jorge Basulto <meeroot98@gmail.com>
2025-12-22 08:50:03 -0500

    agregandol script

---
+++ script.js
@@ -0,0 +1,15 @@
+const formulario = document.querySelector('.Formulario');
+
+formulario.
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+const alumno = document.getElementById('nombre').value;
+const email = document.getElementById('correo').value;
+    console.log(alumno);
+
+});
+   
+
+
+
+
